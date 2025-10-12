1f274dde9f7d94c311bb92b3f8eb26c618e7f4fb
OmethKariyawasam <159032631+OmethKariyawasam@users.noreply.github.com>
2025-10-12 23:03:06 +0530

    Try Tailwind CSS ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import ProductCard from "./components/productCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <ProductCard name="Laptop" image="laptop" price="100,000" />
-      <ProductCard name="Mobile Phone" image="phone" price="90,000" />
-      <ProductCard name="Watch" image="watch" price="10,000" />
+      <div className="border w-[600px] h-[600px] bg-gray-400 flex justify-center items-center relative">
+        <div className="w-[500px] h-[500px] bg-yellow-100 flex flex-col justify-center items-center">
+          <div className="w-[100px] h-[100px] bg-blue-600"></div>
+          <div className="w-[100px] h-[100px] bg-red-600 fixed left-[500px] top-[500px]"></div>
+          <div className="w-[100px] h-[100px] bg-green-600"></div>
+          <div className="w-[100px] h-[100px] bg-pink-600 absolute right-[60px] bottom-[60px]"></div>
+        </div>
+      </div>
     </>
   );
 }
