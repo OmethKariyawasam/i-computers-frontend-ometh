fc59c3ac21f360a77ee612877eaa5f59d439db38
OmethKariyawasam <159032631+OmethKariyawasam@users.noreply.github.com>
2025-10-19 17:32:48 +0530

    Test hooks and routers ✏️

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
   return (
-    <header>
-      <h1>My Shop</h1>
-      <div></div>
+    <header className="w-full h-fit bg-accent">
+      <div className="flex items-center">
+        <img src="/logo.png" alt="logo" width="120px" height="120px" />
+        <h1>My Shop</h1>
+      </div>
     </header>
   );
 }
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,28 @@
+import Header from "../components/header";
+
+export default function HomePage() {
+  return (
+    <div className="w-full h-full">
+      <Header />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi quaerat
+        laudantium libero dolore ipsam? Officia unde doloribus eos fugit
+        corporis culpa rerum ab perferendis distinctio, odit quod aliquid quae
+        est consequatur quo sequi autem ad expedita voluptatum quas voluptas, et
+        incidunt! Excepturi quis quo ipsa eaque aut blanditiis dolorem esse,
+        magni ut possimus voluptatibus, nisi iusto inventore velit modi
+        perspiciatis commodi officia. Illum ipsa veritatis sit adipisci error
+        corrupti temporibus ut commodi doloremque nobis itaque obcaecati dolorem
+        voluptates tenetur qui, cumque ex quo voluptatibus architecto
+        asperiores. Laboriosam nemo impedit provident inventore fugit mollitia
+        enim odit ea aut dignissimos earum necessitatibus qui veniam at quas,
+        vero accusantium ut exercitationem quibusdam dicta unde est! Illum,
+        nulla cum aperiam beatae itaque ipsum in unde eaque alias nostrum harum,
+        dolorem tenetur sit animi id aliquam consequuntur quisquam! Natus atque
+        repudiandae soluta eveniet fuga, incidunt vel exercitationem.
+        Voluptatem, aspernatur tempora reiciendis veniam fuga praesentium
+        corrupti.
+      </p>
+    </div>
+  );
+}
